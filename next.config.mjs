8f0961db0fb3007ee5d721c43dc09e31907aa663
Timeo Williams <timwillie73@gmail.com>
2024-02-10 16:53:04 -0500

    fix: disable server minification.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /* ... */
+  experimental: {
+    serverMinification: false,
+  },
+  /* ... */
+};
 
-export default nextConfig;
+module.exports = nextConfig;
